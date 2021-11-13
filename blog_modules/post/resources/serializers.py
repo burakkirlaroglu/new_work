25326758a6c8a7a42c928dc53433454c9273092c
burakkir <burakkirlaroglu@gmail.com>
2021-11-13 16:02:21 +0300

    ModelSerializer added

--- blog_modules/post/resources/serializers.py
+++ blog_modules/post/resources/serializers.py
@@ -1,20 +1,31 @@
 from rest_framework import serializers
 from blog_modules.post.models import Post
-from django.utils.timezone import now
 
 
-# class PostSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Post
+class PostSerializer(serializers.ModelSerializer):
+    active_post_count = serializers.SerializerMethodField()
+    user_name = serializers.SerializerMethodField()
 
-class PostSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=120)
-    content = serializers.CharField()
-    is_active = serializers.BooleanField()
-    created_date = serializers.DateTimeField()
-    post_count = serializers.SerializerMethodField()
+    class Meta:
+        model = Post
+        fields = ("title", "content", "created_date", "active_post_count", "user_name")
 
-    def get_post_count(self, obj):
+    def get_active_post_count(self, obj):
         """Sum of Posts that is_active field True"""
         qs = Post.objects.all().filter(is_active=True).count()
         return qs
+
+    def get_user_name(self, obj):
+        return obj.user.username
+
+# class PostSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=120)
+#     content = serializers.CharField()
+#     is_active = serializers.BooleanField()
+#     created_date = serializers.DateTimeField()
+#     post_count = serializers.SerializerMethodField()
+#
+#     def get_post_count(self, obj):
+#         """Sum of Posts that is_active field True"""
+#         qs = Post.objects.all().filter(is_active=True).count()
+#         return qs
