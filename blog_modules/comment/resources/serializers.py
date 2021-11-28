dcad9b9407f985f224eb444e61435558cc51fefe
burakkir <burakkirlaroglu@gmail.com>
2021-11-28 12:44:04 +0300

    inner serializer and comment queryset refactored

--- blog_modules/comment/resources/serializers.py
+++ blog_modules/comment/resources/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 
 from blog_modules.comment.models import Comment
+from django.contrib.auth.models import User
+
+from blog_modules.post.models import Post
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -21,12 +24,27 @@ class CommentSerializer(serializers.ModelSerializer):
         return obj.user.username
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ('password',)
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
 class CommandChildSerializer(serializers.ModelSerializer):
     child = serializers.SerializerMethodField()
+    user = UserSerializer()
+    post = PostSerializer()
 
     class Meta:
         model = Comment
         fields = '__all__'
+        # depth = 1 # foreign key'in pk değeri yerine sahip olduğu tüm değerleri gösterir
 
     def get_child(self, obj):
         if obj.any_children:
--- blog_modules/comment/resources/views.py
+++ blog_modules/comment/resources/views.py
@@ -3,6 +3,7 @@ from rest_framework.generics import CreateAPIView, ListAPIView, DestroyAPIView, 
 
 from blog_modules.base.permissions import IsOwner
 from blog_modules.comment.models import Comment
+from blog_modules.comment.pagination import CommentPagination
 from blog_modules.comment.resources.serializers import CommentSerializer, \
     CommandChildSerializer
 
@@ -19,9 +20,22 @@ class CommentApiViews(CreateAPIView):
 class CommentListApiView(ListAPIView):
     # queryset = Comment.objects.all()
     serializer_class = CommandChildSerializer
+    pagination_class = CommentPagination
+
+    # def get_queryset(self):
+    #     return Comment.objects.filter(parent=None)
 
     def get_queryset(self):
-        return Comment.objects.filter(parent=None)
+        """atılan istek çinde q var mı ?
+        varsa queryset i filtrele post id'si q'dan sonraki değer ne ise o parent'a
+        ait postları dön """
+        queryset = Comment.objects.filter(parent=None)
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(post=query)
+        return queryset
+
+
 
 
 class CommentDestroyApiView(DestroyAPIView):
