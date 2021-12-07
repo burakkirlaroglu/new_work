aa1ea238502af45a15fbf6894a6ab531301a011d
burakkir <burakkirlaroglu@gmail.com>
2021-12-07 21:11:28 +0300

    favourite module test file ready for case

--- blog_modules/favourite/tests.py
+++ blog_modules/favourite/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+
+
+class FavouriteAPITestCase(APITestCase):
+
+    def setUp(self) -> None:
+        self.fav_url = reverse("favourite:create-list")
+
+    def test_add_fav_success(self):
+        pass
+
 
 
